e4cb601a8b1cad1211ba56f49e4f1c226b0d3a62
Piotr Lewandowski <lewandowskipiotr02@gmail.com>
2022-06-24 00:37:35 +0200

    #10 try too create s3 bucket

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -1,8 +1,18 @@
 terraform {
   required_providers {
     aws = {
+      region  = "eu-central-1"
       source  = "hashicorp/aws"
       version = "~> 3.0"
+    }
+  }
+
+  resource "aws_s3_bucket" "ngstatic" {
+    bucket = "sylius-plugin-skel-gen-ng01-prod"
+
+    tags = {
+        Name        = "Sylius Plugin Skeleton Generator - Angular webhosting"
+        Environment = "Production"
     }
   }
 }
